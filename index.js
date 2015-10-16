ba29a2b7cd422f0d4262a175258857e35477eac0
sookool99 <shawnmacburnie@gmail.com>
2015-10-16 16:29:16 -0300

    testCommit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 'use strict';
 var pwd = process.cwd(),
-	workingDirString = '--git-dir=' + pwd + '/.git --work-tree=' + pwd,
+    workingDirString = '--git-dir=' + pwd + '/.git --work-tree=' + pwd,
     argv = process.argv,
     runTime = false,
-    exec = require('child_process').exec;
+    exec = require('child_process')
+        .exec;
 if (argv.length > 2) {
     if (argv[2] === 'time' && argv.length > 3) {
         runTime = true;
@@ -16,16 +17,29 @@ if (argv.length > 2) {
         return;
     }
 }
-if (runTime) {
-    setInterval(function () {
-        exec('git ' + workingDirString + ' status',function (error, stdout){
-        	if(error) {
-        		return console.log(error);
-        	}
-        	console.log(stdout);
 
-        });
-    }, parseInt(argv[3]));
+function pushChanges() {
+    exec('git ' + workingDirString + ' add -A', function (error, stdout) {
+        if (error) {
+            return console.log(error);
+        }
+        exec('git ' + workingDirString + ' commit -m "testCommit"',
+            function (error2) {
+                if (error2) {
+                    return console.log(error2);
+                }
+                exec('git ' + workingDirString + ' push', function (error3) {
+                    if (error3) {
+                        return console.log(error2);
+                    }
+                    console.log('Changes commited at: ' + new Date());
+                });
+            });
+        // console.log(stdout);
+    });
+}
+if (runTime) {
+    setInterval(pushChanges, parseInt(argv[3]));
 } else {
     console.log('doing fileChange');
 }
