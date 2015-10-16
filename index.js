39af7268f84dc199549f99868fa088e79a21835c
sookool99 <shawnmacburnie@gmail.com>
2015-10-16 15:51:26 -0300

    added time delay logic

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log('test');
+var pwd = process.cwd(),
+    argv = process.argv,
+    runTime = false,
+    exec = require('child_process').exec;
+if (argv.length > 2) {
+    if (argv[2] === 'time' && argv.length > 3) {
+        runTime = true;
+    } else if (argv[2] === 'change') {
+        runTime = false;
+    } else {
+        console.log('Not a valid command');
+        return;
+    }
+}
+if (runTime) {
+    setInterval(function () {
+        exec('git status',function (error, stdout){
+        	if(error) {
+        		return console.log(error);
+        	}
+        	console.log(stdout);
+
+        });
+    }, parseInt(argv[3]));
+} else {
+    console.log('doing fileChange');
+}
