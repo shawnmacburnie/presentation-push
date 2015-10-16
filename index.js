d850c9f10bead3ab6818c5b4bd08ce686d4ab47e
sookool99 <shawnmacburnie@gmail.com>
2015-10-16 16:45:14 -0300

    5000

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ var pwd = process.cwd(),
     workingDirString = '--git-dir=' + pwd + '/.git --work-tree=' + pwd,
     argv = process.argv,
     runTime = false,
+    commitMessage = false,
     exec = require('child_process')
         .exec;
 if (argv.length > 2) {
     if (argv[2] === 'time' && argv.length > 3) {
+        if (argv.length === 4) {
+            commitMessage = true;
+        }
         runTime = true;
     } else if (argv[2] === 'change') {
+        if (argv.length === 3) {
+            commitMessage = true;
+        }
         runTime = false;
     } else {
         console.log('Not a valid command');
@@ -19,15 +26,20 @@ if (argv.length > 2) {
 }
 
 function pushChanges() {
-	
-    exec('git ' + workingDirString + ' add -A', function (error, stdout) {
+    var message = '';
+    if (commitMessage) {
+        message = argv[argv.length - 1];
+    } else {
+        message = 'presentation-pusher updated repo.';
+    }
+    exec('git ' + workingDirString + ' add -A', function (error) {
         if (error) {
             return console.log(error);
         }
-        exec('git ' + workingDirString + ' commit -m "testCommit"',
+        exec('git ' + workingDirString + ' commit -m "' + message + '"',
             function (error2) {
                 if (error2) {
-                    return;
+                    return console.log('Nothing to commit.');
                 }
                 exec('git ' + workingDirString + ' push', function (error3) {
                     if (error3) {
@@ -36,11 +48,10 @@ function pushChanges() {
                     console.log('Changes commited at: ' + new Date());
                 });
             });
-        // console.log(stdout);
     });
 }
 if (runTime) {
     setInterval(pushChanges, parseInt(argv[3]));
 } else {
-    console.log('doing fileChange');
+    console.log('File Change Feature not yet added.');
 }
