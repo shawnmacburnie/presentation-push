6056dcf9efcfcec13af306f6c3cb35125d187aa4
sookool99 <shawnmacburnie@gmail.com>
2015-11-24 13:20:34 -0400

    presentation-pusher updated repo.

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ var pwd = process.cwd(),
     commitMessage = false,
     defaultTime = 5,
     exec = require('child_process')
-        .exec;
+        .exec,
+    message = '';
+
+function pushChanges() {
+    exec('git ' + workingDirString + ' add -A', function (error) {
+        if (error) {
+            return console.log(error);
+        }
+        exec('git ' + workingDirString + ' commit -m "' + message + '"',
+            function (error2) {
+                if (error2) {
+                    return console.log('Nothing to commit.');
+                }
+                exec('git ' + workingDirString + ' push', function (error3) {
+                    if (error3) {
+                        return console.log(error2);
+                    }
+                    console.log('Changes commited at: ' + new Date());
+                });
+            });
+    });
+}
 if (argv.length > 2) {
     if (argv[2] === 'time') {
         if (argv.length > 3) {
@@ -28,34 +49,14 @@ if (argv.length > 2) {
         return;
     }
 }
-
-function pushChanges() {
-    var message = '';
-    if (commitMessage) {
-        message = argv[argv.length - 1];
-    } else {
-        message = 'presentation-pusher updated repo.';
-    }
-    exec('git ' + workingDirString + ' add -A', function (error) {
-        if (error) {
-            return console.log(error);
-        }
-        exec('git ' + workingDirString + ' commit -m "' + message + '"',
-            function (error2) {
-                if (error2) {
-                    return console.log('Nothing to commit.');
-                }
-                exec('git ' + workingDirString + ' push', function (error3) {
-                    if (error3) {
-                        return console.log(error2);
-                    }
-                    console.log('Changes commited at: ' + new Date());
-                });
-            });
-    });
+if (commitMessage) {
+    message = argv[argv.length - 1];
+} else {
+    message = 'presentation-pusher updated repo.';
 }
 if (runTime) {
     console.log('Running Timed commits every ' + defaultTime + ' minutes.');
+    console.log('With commit message: ' + message + '\n');
     setInterval(pushChanges, defaultTime * 60000);
 } else {
     console.log('File Change Feature not yet added.');
